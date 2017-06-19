d956868c69bba9ef9fdf696865c02e3bb675a8ee
doooon <doooon>
2017-06-20 03:40:36 +0900

    午前3時40分

--- LIB_過去回答.js
+++ LIB_過去回答.js
@@ -748,15 +748,9 @@ if (TEXT.match(/^[\.\-\s]+$/)) {
 }
 
 
-<<<<<<< HEAD
 //元素記号からdecASCII 元素記号２文字優先
 htmlTmp.push(TEXT);
 htmlTmp.push("(原子番号 元素記号２文字優先)");
-=======
-//元素記号からdecASCII (最大)
-htmlTmp.push(TEXT);
-htmlTmp.push("(原子番号 大きい並び順)");
->>>>>>> origin/master
 function atomOut(L) {
   var tmp2="<table id='atom'><tr>";
   for (var i in L[1]) {
@@ -799,16 +793,9 @@ atomOut(tmp);
 htmlTmp.push("===============");
 
 
-<<<<<<< HEAD
-
 //元素記号からdecASCII 元素記号1文字優先
 htmlTmp.push(TEXT);
 htmlTmp.push("(原子番号 元素記号1文字優先)");
-=======
-//元素記号からdecASCII (最小)
-htmlTmp.push(TEXT);
-htmlTmp.push("(原子番号 小さい並び順)");
->>>>>>> origin/master
 function atomOut(L) {
   var tmp2="<table id='atom'><tr>";
   for (var i in L[1]) {
