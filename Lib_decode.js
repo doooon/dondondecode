f8f54bfbdc0495e29e81cc40acaa46f5ee454f35
doooon <doooon>
2017-06-21 21:30:20 +0900

    21時30分

--- Lib_decode.js
+++ Lib_decode.js
@@ -5148,6 +5148,12 @@ function keySyougou(source, len, terget) {
     }
     if (flag) result.push(keys[i]);
   } // for keys.length
+  result.sort(function(a,b){
+    if( a.length > b.length) return -1;
+    if( a.length < b.length) return 1;
+    return 0;
+  });
+
   return result;
 } // end function
 
