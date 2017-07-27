2110f6c06f0875deb75049b09d1f55d1cc20e461
doooon <doooon.ingress@gmail.com>
2017-07-27 10:20:45 +0900

    Update LIB_過去回答6.js

--- LIB_過去回答6.js
+++ LIB_過去回答6.js
@@ -22,7 +22,12 @@ if (
   htmlCode(result.join(""));
   htmlTmp.push("<b>(polybius)</b>");
   htmlCode(polybius(result.join("")));
-  htmlTmp.push("==============");
+  
+  var tmp2=polybius(result.join(""));
+  htmlTmp.push(tmp2);
+  htmlCode(tmp2);
+
+htmlTmp.push("==============");
 }
 
 
