5ebc09a690fe38b21e58ca95cdb7494096eb0a7c
doooon.ingress <doooon.ingress@gmail.com>
2018-01-09 02:08:22 +0900

    8

--- Lib_decode_func.js
+++ Lib_decode_func.js
@@ -1602,62 +1602,11 @@ function goRectangles(str) {
   if ((TEXT.length+"").match(/^(6|10|15|21|28|36|45|55)$/)) {
     htmlTmp.push("ピラミッド型");
     
-    debug("77777");
+    debug("8");
     /*
-    function pyramidRect(tmp,isReverse){
-      let result=[];
 
-      if ((tmp.length+"").match(/^55$/)) {
-        tmp=tmp.replace(/^(.{10})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^45$/)) {
-        tmp=tmp.replace(/^(.{9})(.*)$/, 
-          (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^36$/)) {
-        tmp=tmp.replace(/^(.{8})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^28$/)) {
-        tmp=tmp.replace(/^(.{7})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^21$/)) {
-        tmp=tmp.replace(/^(.{6})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^15$/)) {
-        tmp=tmp.replace(/^(.{5})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^10$/)) {
-        tmp=tmp.replace(/^(.{4})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^6$/)) {
-        tmp=tmp.replace(/^(.{3})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^3$/)) {
-        tmp=tmp.replace(/^(.{2})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
-      if ((tmp.length+"").match(/^1$/)) {
-        tmp=tmp.replace(/^(.{1})(.*)$/, 
-            (m,p1,p2)=>{result.push(p1);return p2});
-      }
 
-      if (isReverse.match(/^reverse$/i)) {
-        result.reverse();
-        result.forEach((v,i,o)=>{
-          o[i]=v.split("").reverse().join("");
-        });
-      }
-      return result;
-    }
 
-   
     
     let result=[];
     result=pyramidRect(TEXT);
@@ -1722,6 +1671,60 @@ function goRectangles(str) {
   
 
 } // end function
+
+//ピラミッド型に並び替え
+function pyramidRect(tmp,isReverse){
+      let result=[];
+
+      if ((tmp.length+"").match(/^55$/)) {
+        tmp=tmp.replace(/^(.{10})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^45$/)) {
+        tmp=tmp.replace(/^(.{9})(.*)$/, 
+          (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^36$/)) {
+        tmp=tmp.replace(/^(.{8})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^28$/)) {
+        tmp=tmp.replace(/^(.{7})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^21$/)) {
+        tmp=tmp.replace(/^(.{6})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^15$/)) {
+        tmp=tmp.replace(/^(.{5})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^10$/)) {
+        tmp=tmp.replace(/^(.{4})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^6$/)) {
+        tmp=tmp.replace(/^(.{3})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^3$/)) {
+        tmp=tmp.replace(/^(.{2})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+      if ((tmp.length+"").match(/^1$/)) {
+        tmp=tmp.replace(/^(.{1})(.*)$/, 
+            (m,p1,p2)=>{result.push(p1);return p2});
+      }
+
+      if (isReverse.match(/^reverse$/i)) {
+        result.reverse();
+        result.forEach((v,i,o)=>{
+          o[i]=v.split("").reverse().join("");
+        });
+      }
+      return result;
+} // end func
 
 
 // skip
