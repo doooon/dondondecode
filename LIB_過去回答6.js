f5c6bc48dfc16e79ef9637aff8ea0ebb3b04f320
doooon <doooon.ingress@gmail.com>
2018-01-16 17:57:21 +0900

    fix

--- LIB_過去回答6.js
+++ LIB_過去回答6.js
@@ -43,7 +43,7 @@ if (
 tmp=tmp.map(v=>[v.toUpperCase().charCodeAt(0)-65,v.toUpperCase().charCodeAt(1)-65]);
   let tmp2=[];
   tmp.forEach(v=>tmp2.push(v[0]-v[1]));
-  htmlTmp.push(tm2p.join(" "));
+  htmlTmp.push(tmp2.join(" "));
 
   tmp=tmp.map(v=>v[0]*26+v[1]);
   htmlCode(tmp.join(""));
