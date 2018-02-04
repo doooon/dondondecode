d9fac30b047efa529141534697fed20cfe2a1e60
doooon.ingress <doooon.ingress@gmail.com>
2018-02-04 15:52:57 +0900

    up

--- Lib_decode_htmlCode.js
+++ Lib_decode_htmlCode.js
@@ -1782,14 +1782,6 @@ function checkCodeHTML(str, noA) {
       var re2=new RegExp("^"+kList[0]+"$", "i");
       var sstr=str.split(re);
 
-      // debug
-      if (str.match(/sigint/i)) {
-        debug(`leetLessKw = ${leetLessKw}
-kList[0] = ${kList[0]}
-sstr[0] = ${sstr[0]}
-sstr[1] = ${sstr[1]}`);
-      }
-
       for (var i=0; i<sstr.length; i=i+1) {
         if (!sstr[i]) {
           sstr.splice(i,1); //空の配列を削除
