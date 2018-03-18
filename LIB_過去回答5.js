659b59e55c10e2b1d29f1da2c00b83d7aaa8f831
doooon.ingress <doooon.ingress@gmail.com>
2018-03-18 11:50:50 +0900

    f

--- LIB_過去回答5.js
+++ LIB_過去回答5.js
@@ -236,7 +236,7 @@ if (
   var tmpRE=
     new RegExp(RegExp.$1, "g");
   var tmp=TEXT.replace(tmpRE, " ");
-  tmp=tmp.replace(/ +/g," ")
+  //tmp=tmp.replace(/ +/g," ")
   tmp=tmp.replace(/a/ig, ".");
   tmp=tmp.replace(/b/ig, "..");  
   tmp=tmp.replace(/c/ig, "...");  
