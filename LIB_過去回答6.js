40bcf59c28fa70e1f073427b74aea9724515a534
doooon.ingress <doooon.ingress@gmail.com>
2019-02-24 16:25:22 +0900

    f

--- LIB_過去回答6.js
+++ LIB_過去回答6.js
@@ -55,7 +55,7 @@ if (
   for (var i=0; i<tmp.length; i++) {
     var j= (tmp.length-1) - i;
     result.push(Number(tmp[i])+j);
-    htmlTmp.push("["+i+"] "+tmp[i]+" + "+j+" = "+ (Number(tmp[i])+j )+("+(tmp.length-1)+" - "+i+")");
+    htmlTmp.push(`[${i}] ${tmp[i]}+${j}= ${Number(tmp[i])+j} (${j}= ${tmp.length-1} - ${i})`);
   }
   htmlCode(result.join(""));
   htmlTmp.push("==============");
