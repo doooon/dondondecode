ed4ff9f433bb8d660668865f961689d8a1b33b0e
doooon <doooon.ingress@gmail.com>
2020-10-20 17:16:42 +0900

    fix

--- Lib_一括.js
+++ Lib_一括.js
@@ -1032,7 +1032,7 @@ if (
   htmlCode(strReverse(tmp));
   htmlTmp.push(
     "(more reverse &amp; base32)");
-  htmlCode(base32Dec(tmstrReverse(tmp)));
+  htmlCode(base32Dec(strReverse(tmp)));
 
   htmlTmp.push(
     "(more base32)");
